1b7eedb9a448716ef92cc7ee0c00ae78f227c3d2
boluozai-ai <81038688+boluozai-ai@users.noreply.github.com>
2021-05-16 23:17:29 -0500

    Create 1283.py

---
+++ 1283.py
@@ -0,0 +1,13 @@
+class Solution:
+    def smallestDivisor(self, nums: List[int], threshold: int) -> int:
+        l, r = 1, max(nums)
+        
+        while l < r:
+            mid = l + (r - l) // 2
+            if sum(ceil(num/mid) for num in nums) > threshold:
+                l = mid + 1
+            else:
+                r = mid
+                
+        return l
+        
